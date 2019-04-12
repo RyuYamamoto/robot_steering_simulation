405c0832c9244b5bb5989d746e76f1bcc5658789
ryu.yamamoto <ryu.yamamoto@softbankrobotics.com>
2019-04-12 13:15:40 +0900

    enable simulation

--- robot_steering_sim.py
+++ robot_steering_sim.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import math
 import matplotlib.patches as patches
 import numpy as np
+import matplotlib.animation as animation
 from scipy.interpolate import interp1d
 
 # サンプリング周期[s]
@@ -23,15 +24,22 @@ class Visualize:
         self.way_point_x = []
         self.way_point_y = []
 
+        self.fig = plt.figure()
+        self.ims = []
         self.ax = plt.axes()
 
     def config_screen(self):
         self.ax.cla()
         self.ax.axis('equal')
         self.ax.set_xlim(-1, 7)
-        self.ax.set_ylim(-5, 5)
+        self.ax.set_ylim(-6, 6)
         self.ax.set_xlabel("X [m]", fontsize=20)
         self.ax.set_ylabel("Y [m]", fontsize=20)
+
+    def save_gif(self):
+        ani = animation.ArtistAnimation(self.fig, self.ims, interval=50, blit=True,
+                                        repeat_delay=1000)
+        ani.save('anim.gif', writer="imagemagick")
 
     def move_robot(self, pos):
         x, y, theta = pos
@@ -47,13 +55,17 @@ class Visualize:
         c = patches.Circle(xy=(x, y), radius=self.r, fill=False, color="black")
         self.ax.add_patch(c)
 
-    def set_way_point(self, way_point_x, way_point_y):
-        self.way_point_x = way_point_x
-        self.way_point_y = way_point_y
+    def set_way_point(self, way_point_list):
+        self.way_point_x, self.way_point_y = interpolation(
+            way_point_list)
+        self.way_point_list = way_point_list
 
     def draw_way_point(self):
         self.ax.scatter(self.way_point_x, self.way_point_y,
-                        marker="*", s=5, color="black")
+                        marker="o", s=2, color="red")
+        for way_point in self.way_point_list:
+            self.ax.scatter(way_point[0], way_point[1],
+                            marker="*", s=20, color="black")
 
     def draw_coordinate(self, pose):
         x, y, theta = pose
@@ -74,7 +86,7 @@ class Visualize:
         self.robot_pos_x_list.append(x)
         self.robot_pos_y_list.append(y)
         self.ax.scatter(self.robot_pos_x_list,
-                        self.robot_pos_y_list, s=5, color="blue", label="trajectory")
+                        self.robot_pos_y_list, s=1, color="blue", label="trajectory")
         self.ax.legend()
 
 # ロボットの移動時に正規分布に基づくノイズをのせる
@@ -83,37 +95,39 @@ class Visualize:
 def noise(u, w):
     print("正規分布")
 
+# 一次補間 or スプライン補間??
+    # サンプリング周期DTで参照位置を離散化する
+
+
+def interpolation(way_point_list, alg="cubic"):
+    ix = []
+    iy = []
+
+    temp_x = temp_y = np.array([])
+    for way_point in way_point_list:
+        temp_x = np.append(temp_x, way_point[0])
+        temp_y = np.append(temp_y, way_point[1])
+    # スプライン補間
+    cubic_spline = None
+    if alg == "linear":
+        cubic_spline = interp1d(temp_x, temp_y)
+    # 線形補間
+    elif alg == "cubic":
+        cubic_spline = interp1d(temp_x, temp_y, kind='cubic')
+
+    ix = np.linspace(
+        way_point_list[0][0], way_point_list[-1][0], num=700)
+    iy = cubic_spline(ix)
+
+    return ix, iy
+
 
 class MotionControl:
     def __init__(self, way_point_list):
         self.v = 0.
-        self.look_ahead = 1.0
+        self.look_ahead = 2.0
         self.way_point_list = way_point_list
-        self.interpolation()
-
-        print(len(self.ix))
-
-    # 一次補間 or スプライン補間??
-    # サンプリング周期DTで参照位置を離散化する
-    def interpolation(self, alg="cubic"):
-        self.ix = []
-        self.iy = []
-
-        temp_x = temp_y = np.array([])
-        for way_point in way_point_list:
-            temp_x = np.append(temp_x, way_point[0])
-            temp_y = np.append(temp_y, way_point[1])
-        # スプライン補間
-        cubic_spline = None
-        if alg == "linear":
-            cubic_spline = interp1d(temp_x, temp_y)
-        # 線形補間
-        elif alg == "cubic":
-            cubic_spline = interp1d(temp_x, temp_y, kind='cubic')
-
-        self.ix = np.linspace(
-            way_point_list[0][0], way_point_list[-1][0], num=700)
-        self.iy = cubic_spline(self.ix)
+        self.ix, self.iy = interpolation(way_point_list)
 
     # ロボットの速度が目標の速度へ追従するように比例制御(P制御)を行う
     def pid_velocity_control(self, target_velocity):
@@ -123,48 +137,37 @@ class MotionControl:
     def seach_reference_point(self, state):
         try:
             x, y, theta = state
-            index = 0
-            distance = 0.1 * self.v + 1.0
-            self.look_ahead = 0
-            while index <= len(self.ix)-1:
-                dx = self.ix[index] - x
-                dy = self.iy[index] - y
-                self.look_ahead = math.sqrt(pow(dx, 2) + pow(dy, 2))
+            dx = [x - t_ix for t_ix in self.ix]
+            dy = [y - t_iy for t_iy in self.iy]
+            distance = [abs(math.sqrt(pow(idx, 2) + pow(idy, 2)))
+                        for (idx, idy) in zip(dx, dy)]
+            index = distance.index(min(distance))
+            self.look_ahead = 0.
+            Lf = 0.1 * self.v + 0.3
+
+            while Lf > self.look_ahead and (index + 1) < len(self.ix):
+                dx = self.ix[index + 1] - self.ix[index]
+                dy = self.iy[index + 1] - self.iy[index]
+                self.look_ahead += math.sqrt(dx ** 2 + dy ** 2)
                 index += 1
-                if distance < self.look_ahead:
-                    return index-1, distance
-                if len(self.ix)-1 <= index:
-                    break
+            return index
         except Exception as message:
             print(str(message))
-        # 指定したlook ahead distanceに満たない場合は最も距離が近い
-        # リファレンスデータを参照するようにする
-        #dx = [x - t_ix for t_ix in self.ix]
-        #dy = [y - t_iy for t_iy in self.iy]
-        # self.look_ahead = [math.sqrt(pow(idx, 2) + pow(idy, 2))
-        #                   for (idx, idy) in zip(dx, dy)]
-        # return distance.index(min(distance)), min(distance)
 
     # pure pursuitによりステアンリング角度を計算する
     def calc_pure_pursuit(self, state):
         x, y, theta = state
         # リファレンスパスを検索する
-        index, self.look_ahead = self.seach_reference_point(state)
-        x_ref = self.ix[index]
-        y_ref = self.iy[index]
+        index = self.seach_reference_point(state)
 
         # 方位誤差を計算する
-        alpha = math.atan2(y_ref - y, x_ref - x) - theta
-
-        if self.v < 0:  # back
-            alpha = math.pi - alpha
+        alpha = math.atan2(self.iy[index] - y, self.ix[index] - x) - theta
 
         # ステアリング角を計算する
-        delta = math.atan2(2.0 * self.look_ahead *
-                           math.sin(alpha) / (1.0 + 0.1 * self.v), 1.0)
-        print("look ahead: {}".format(self.look_ahead))
+        steering_angle = math.atan2(2.0 * self.look_ahead *
+                                    math.sin(alpha) / (0.3 + 0.1 * self.v), 1.0)
 
-        return delta
+        return steering_angle, index
 
     # ロボットの運動モデルにはKinematics Modelを使用する
     # 前進速度とステアリング角によりt+1後の位置を計算する
@@ -176,7 +179,6 @@ class MotionControl:
         y = y + self.v * math.sin(theta) * DT
         theta = theta + (self.v / self.look_ahead) * \
             math.tan(steering_angle) * DT
-        print("theta: {}".format(theta))
         self.v = self.v + ak * DT
 
         return [x, y, theta]
@@ -186,16 +188,25 @@ way_point_list = [[0, 0], [0.5, 0.5], [1.5, -0.2],
                   [2.7, -0.6], [4.0, 0.1], [6.3, -0.4]]
 
 
+def check_goal(robot_pos):
+    threshold = 0.1
+    x, y, _ = robot_pos
+    if (way_point_list[-1][0] - threshold < x < way_point_list[-1][0] + threshold) and (way_point_list[-1][1] - threshold < y < way_point_list[-1][1] + threshold):
+        return True
+    else:
+        return False
+
+
 if __name__ == "__main__":
     controller = MotionControl(way_point_list)
     visualize = Visualize()
 
     # ロボットの位置 [m]
-    robot_pos = np.array([0, 0.5, 0])
+    robot_pos = np.array([-0.1, 0.5, 0])
     # ロボットの目標速度 [m/s]
-    target_velocity = 1.5
+    target_velocity = 7.5
 
-    visualize.set_way_point(controller.ix, controller.iy)
+    visualize.set_way_point(way_point_list)
 
     count = 0
     print("simulation start.")
@@ -207,10 +218,12 @@ if __name__ == "__main__":
         # P制御により計算されたロボットの速度
         ak = controller.pid_velocity_control(target_velocity)
         # pure pursitにより計算されたステアリング角
-        steering_angle = controller.calc_pure_pursuit(robot_pos)
-        # 方位、前進速度、現在位置から次の位置を更新する
-        robot_pos = controller.steering_control(
-            robot_pos, ak, steering_angle)
+        steering_angle, index = controller.calc_pure_pursuit(robot_pos)
+
+        if check_goal(robot_pos) is False:
+            # 方位、前進速度、現在位置から次の位置を更新する
+            robot_pos = controller.steering_control(
+                robot_pos, ak, steering_angle)
 
         # ロボットを描画する
         visualize.move_robot(robot_pos)
